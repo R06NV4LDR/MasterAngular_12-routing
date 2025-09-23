05310700759c0ddb788081ca091f960a2f550ea4
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-23 11:32:45 +0200

    294 | Class-based Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 
 import { routes as userRoutes } from "./users/users.routes";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import {
+  resolveTitle,
+  resolveUserName,
+  UserTasksComponent,
+} from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl("/unauthorized"));
+};
 
 export const routes: Routes = [
   {
@@ -11,19 +25,21 @@ export const routes: Routes = [
     component: NoTaskComponent,
     // redirectTo: 'users/u1',
     // pathMatch: 'full'
-    title: 'No task selected'
+    title: "No task selected",
   },
   {
     path: "users/:userId", // <your-domain.com>/users/<uid>/
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
+
     data: {
       message: "Hello!",
     },
     resolve: {
-      userName: resolveUserName
+      userName: resolveUserName,
     },
-    title: resolveTitle
+    title: resolveTitle,
   },
   {
     path: "**",
